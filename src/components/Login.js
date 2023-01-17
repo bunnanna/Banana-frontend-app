6c6ede091730a7232213585c36535357e2b08c16
bunnanna <na.nuttanont@gmail.com>
2023-01-17 11:35:39 +0900

    adj task list

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,29 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
+import { useGetUsersQuery } from "../features/users/usersApiSlice";
 
 const Login = () => {
+    const usernameRef = useRef()
+    const passwordRef = useRef()
+
+    const onLogin = e =>{
+        e.preventDefault()
+        
+
+    }
+
     return ( 
         <div><h1>Login</h1> 
-        <Link to="/main"><h2> Complete!!!?</h2></Link></div>
+        <Link to="/main"><h2> Complete!!!?</h2></Link>
+        <div className="login__layout">
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" value={usernameRef.current}/>
+
+            <label htmlFor="password">Password</label>
+            <input type="text" name="password" value={passwordRef.current}/>
+        </div>
+        <button ></button>
+        </div>
      );
 }
  
